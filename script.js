0443b7a14e37702de3b0d8fb03365a8e416f72f4
ysaturn00 <idanixbox@gmail.com>
2025-04-21 17:40:27 -0300

    new

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ document.getElementById('loginForm').addEventListener('submit', function(e) {
     const password = document.getElementById('password').value;
     
     if(username && password) {
-        // Aqui você pode colocar alguma lógica real de autenticação no futuro
         window.location.href = "dashboard.html";
     } else {
         alert('Por favor, preencha todos os campos.');
